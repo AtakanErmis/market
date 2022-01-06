df7bf683f5a0cae07716589751f0dcf18c76b9c0
Atakan Ermiş <me@atakanermis.com>
2022-01-06 23:03:00 +0300

    feat: write interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,26 @@
+export interface ICompany {
+  slug: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  account: number;
+  contact: string;
+}
+
+export interface IProduct {
+  tags: string[];
+  price: number;
+  name: string;
+  description: string;
+  slug: string;
+  added: number;
+  manufacturer: string;
+  itemType: string;
+}
+
+export interface ISelectItem {
+  name: string;
+  count?: number;
+}
