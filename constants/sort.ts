f596fa5d68644e98f44a22a3daf31b3ac36defbc
Atakan Ermiş <me@atakanermis.com>
2022-01-07 14:36:53 +0300

    feat: add constants and config files

---
+++ constants/sort.ts
@@ -0,0 +1,22 @@
+export const sortItems = [
+  {
+    name: "Price low to high",
+    sortOrder: "asc",
+    sortType: "price",
+  },
+  {
+    name: "Price high to low",
+    sortOrder: "desc",
+    sortType: "price",
+  },
+  {
+    name: "New to old",
+    sortOrder: "desc",
+    sortType: "added",
+  },
+  {
+    name: "Old to new",
+    sortOrder: "asc",
+    sortType: "added",
+  },
+];
