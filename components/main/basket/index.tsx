cd4d55cffc1f52fccd4906c876541bde87c9716a
Atakan Ermiş <me@atakanermis.com>
2022-01-10 01:46:17 +0300

    feat: empty state for Basket component

--- components/main/basket/index.tsx
+++ components/main/basket/index.tsx
@@ -2,22 +2,31 @@ import { useSelector } from "react-redux";
 import { IBasket } from "@interfaces/basket";
 import BasketItem from "@components/main/basket/BasketItem";
 import { AppState } from "@stores/store";
+import BasketIcon from "@assets/icons/basket-large.svg";
 
 // Component for displaying the basket.
 export default function Basket() {
   const basket: IBasket = useSelector((state: AppState) => state.basket);
 
-  if (basket.items.length === 0) return null;
-  return (
-    <ul className="basket">
-      <div>
+  if (basket.items.length) {
+    return (
+      <ul className="basket">
         {basket.items.map((item) => (
           <BasketItem key={item.product.slug} basketItem={item} />
         ))}
         <li className="basket-price-wrapper">
           <div className="basket-price">₺{basket.totalPrice.toFixed(2)}</div>
         </li>
+      </ul>
+    );
+  } else {
+    return (
+      <div className="basket basket-empty">
+        <div className="basket-empty__icon">
+          <BasketIcon />
+        </div>
+        <div className="basket-empty__text">Your basket is empty.</div>
       </div>
-    </ul>
-  );
+    );
+  }
 }
